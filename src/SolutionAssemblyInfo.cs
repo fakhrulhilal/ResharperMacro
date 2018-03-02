1e65ce0e8c85845aff691bcba0944199996ad92d
Fakhrulhilal Maktum <fakhrulhilal@gmail.com>
2018-03-02 16:30:21 +0700

    Add support for R# 2017.3.2

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyVersion("2017.3.2")]
+[assembly: AssemblyFileVersion("2017.3.2")]
